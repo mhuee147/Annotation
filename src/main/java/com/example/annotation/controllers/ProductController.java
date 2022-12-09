05de98443b8d5b4760d8e380b51aedea1e8ab097
MinhHue <minhhue14072002@gmail.com>
2022-12-09 13:47:46 +0700

    annotation @configuration

--- src/main/java/com/example/annotation/controllers/ProductController.java
+++ src/main/java/com/example/annotation/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package com.example.annotation.controllers;
 
+import com.example.annotation.models.Product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,12 @@ import java.util.List;
 public class ProductController {
     //this request is: http://localhost:8080/Product/getAllProducts
     @GetMapping("")
-    List<String> getAllProducts(){
-        return List.of("iphone", "ipad");
+    List<Product> getAllProducts(){
+        return List.of(
+                new Product(1L,"Macbook",2020,2400,""),
+                new Product(2L,"hp",2022,3400,"")
+
+        );
     }
 
 }
